579aea7ea299a069c0bc0ff81d91513462ad6d66
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-12-06 22:10:04 +0200

    updated comments

--- Gade Poe/Gade Poe/Item.cs
+++ Gade Poe/Gade Poe/Item.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Task_1
 {
-    //Q2.1 abstract class item inherits from Tile
+    //abstract class item inherits from Tile
     abstract class Item : Tile
     {
         //constructor receiving x and y positions
@@ -13,7 +13,7 @@ namespace Task_1
             //data goes to Tile through constructor initialiser
             //Otherwise empty
         }
-
+        
         //Abstract to string method that is overridden by subclasses to return items
         public abstract override string ToString();
     }
