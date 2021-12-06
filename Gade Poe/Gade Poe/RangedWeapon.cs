1b6b0f5938aa0a173fae9815309f2c0bd27e34d4
miaaa95 <89992357+miaaa95@users.noreply.github.com>
2021-12-06 21:44:49 +0200

    Updated 2.3

--- Gade Poe/Gade Poe/RangedWeapon.cs
+++ Gade Poe/Gade Poe/RangedWeapon.cs
@@ -4,13 +4,16 @@ using System.Text;
 
 namespace Task_1
 {
+    //Q2.3 RangedWeapon subclass to weapon
     class RangedWeapon : Weapon
     {
+        //public overridden Range accessor that returns base range
         public override int getRange
         {
             get { return Range; }
         }
-
+        
+        //public enum called 'Types'
         public RangedWeapon(Types types, int x = 0, int y = 0) : base('W', x, y)
         {
             if (types == Types.Rifle)
@@ -31,6 +34,7 @@ namespace Task_1
                 getCost = 6;
             }
         }
+        
         public override string ToString()
         {
             return getWeapon_type + " cost: " + getCost;
